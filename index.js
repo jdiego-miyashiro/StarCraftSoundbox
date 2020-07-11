90d09bcb045850ca1c3ece4f609004562c9e8fa6
jdiego-miyashiro <castromi@ualberta.ca>
2020-07-11 08:17:50 -0600

    Finished the navbar

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
 
 $.get("https://srv-file14.gofile.io/download/aQ6ID3/AudioNameList.txt", function(data) {
 
-  var NumberOfDrumButtons = document.querySelectorAll("button").length;
+  var NumberOfDrumButtons = document.querySelectorAll(".unit-portrait").length;
   var AudioNameList = data.split(',');
-
-  function SelectAudio(ArrayofAudioNames, UnitNameString){
-    var SelectedAudioArrays =[];
-    for (var i = 0; i < ArrayofAudioNames.length; i++){
-      if (ArrayofAudioNames[i].includes(UnitNameString)) {
-        SelectedAudioArrays.push(ArrayofAudioNames[i]);} }
-    var j = Math.floor((Math.random() * SelectedAudioArrays.length));
-
-    return SelectedAudioArrays[j];
-  }
 
 
 
   for (var i = 0; i < NumberOfDrumButtons; i++) {
-    var unitname = document.querySelectorAll("button")[i].getAttribute("value");
+    var unitname = document.querySelectorAll(".unit-portrait")[i].getAttribute("value");
     var unitURL = "url(images/"+unitname
-    document.querySelectorAll("button")[i].style.backgroundImage = unitURL+".jpg)"
+    document.querySelectorAll(".unit-portrait")[i].style.backgroundImage = unitURL+".jpg)"
 
-    document.querySelectorAll("button")[i].addEventListener("click", function() {
+    document.querySelectorAll(".unit-portrait")[i].addEventListener("click", function() {
       var SCVAudio = new Audio("Audios/"+SelectAudio(AudioNameList,this.getAttribute("value")));
       SCVAudio.play();
     });
 
-    document.querySelectorAll("button")[i].addEventListener("mouseover", function() {
+    document.querySelectorAll(".unit-portrait")[i].addEventListener("mouseover", function() {
     this.style.backgroundImage ="url(images/"+this.getAttribute("value")+".gif)"
     });
 
-    document.querySelectorAll("button")[i].addEventListener("mouseout", function() {
+    document.querySelectorAll(".unit-portrait")[i].addEventListener("mouseout", function() {
       this.style.backgroundImage ="url(images/"+this.getAttribute("value")+".jpg)"
     });
+
+    function SelectAudio(ArrayofAudioNames, UnitNameString){
+      var SelectedAudioArrays =[];
+      for (var i = 0; i < ArrayofAudioNames.length; i++){
+        if (ArrayofAudioNames[i].includes(UnitNameString)) {
+          SelectedAudioArrays.push(ArrayofAudioNames[i]);} }
+      var j = Math.floor((Math.random() * SelectedAudioArrays.length));
+
+      return SelectedAudioArrays[j];
+    }
 
 
 
